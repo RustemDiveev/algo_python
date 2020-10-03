431b345184f8f039dc0890e5324ce1343eb49b9b
Rustem Diveev <rustem.diveev@gmail.com>
2020-10-03 16:11:30 +0300

    Added r_1_5 (also with iterator) and unit tests

--- ex_1/tests.py
+++ ex_1/tests.py
@@ -10,6 +10,9 @@ from ex_1.r_1_1 import is_multiple
 from ex_1.r_1_2 import is_even
 from ex_1.r_1_3 import minmax
 from ex_1.r_1_4 import get_sum_of_squares
+from ex_1.r_1_5 import get_sum_of_squares_with_sum, \
+    get_sum_of_squares_with_sum_and_iterator
+
 
 class Test_r_1_1(unittest.TestCase):
 
@@ -74,6 +77,40 @@ class Test_r_1_4(unittest.TestCase):
     def test_sum_of_squares_with_input_is_string(self):
         with self.assertRaises(ValueError):
             get_sum_of_squares("blah blah blah, mr Freeman!")
+
+class Test_r_1_5(unittest.TestCase):
+
+    def test_sum_of_squares_with_sum_from_1_to_4(self):
+        v_sum = 1*1 + 2*2 + 3*3 + 4*4
+        self.assertEqual(v_sum, get_sum_of_squares_with_sum(5))
+
+    def test_sum_of_squares_with_sum_from_1_to_9999(self):
+        v_sum = sum([num * num for num in range(1,10000)])
+        self.assertEqual(v_sum, get_sum_of_squares_with_sum(10000))
+
+    def test_sum_of_squares_with_sum_with_input_is_negative_int(self):
+        with self.assertRaises(ValueError):
+            get_sum_of_squares_with_sum(-200)
+
+    def test_sum_of_squares_with_sum_with_input_is_string(self):
+        with self.assertRaises(ValueError):
+            get_sum_of_squares_with_sum("blah blah blah, mr Freeman!")
+
+    def test_sum_of_squares_with_sum_and_iterator_from_1_to_4(self):
+        v_sum = 1*1 + 2*2 + 3*3 + 4*4
+        self.assertEqual(v_sum, get_sum_of_squares_with_sum_and_iterator(5))
+
+    def test_sum_of_squares_with_sum_and_iterator_from_1_to_9999(self):
+        v_sum = sum([num * num for num in range(1,10000)])
+        self.assertEqual(v_sum, get_sum_of_squares_with_sum_and_iterator(10000))
+
+    def test_sum_of_squares_with_sum_and_iterator_with_input_is_negative_int(self):
+        with self.assertRaises(ValueError):
+            get_sum_of_squares_with_sum_and_iterator(-200)
+
+    def test_sum_of_squares_with_sum_and_iterator_with_input_is_string(self):
+        with self.assertRaises(ValueError):
+            get_sum_of_squares_with_sum_and_iterator("blah blah blah, mr Freeman!")
 
 if __name__ == '__main__':
     unittest.main()
