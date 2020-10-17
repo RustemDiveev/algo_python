717ea8fc9dec0962bbf03fca728ef66ae5535c01
Rustem Diveev <rustem.diveev@gmail.com>
2020-10-17 16:02:27 +0300

    Just some thoughts and todos about c_1_20

---
+++ ex_1/c_1_20.py
@@ -0,0 +1,18 @@
+from random import randint
+
+# TODO: Test is shuffle(data) and randint_version_of_shuffle(data) shuffles elements with same probability
+
+def randint_version_of_shuffle(i_list: list) -> list:
+    """
+        Implementation of shuffle function with usage 
+        of random.randint function 
+    """
+    """
+        My version of algorithm (17.10.2020) - maybe there is more effective solution
+        1. Select randomly one of the elements 
+        2. This element will be first 
+        3. Push that element to final list 
+        4. Delete element from original list 
+        5. Move to step 1, until list is empty
+    """
+    pass 
