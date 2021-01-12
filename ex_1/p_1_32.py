6f0ecc0d9a39cb943075a4c73ba0c48599694213
Rustem Diveev <rustem.diveev@gmail.com>
2021-01-12 05:56:19 +0300

    Bydlo version of very simple calculator - tests and refactoring required!

--- ex_1/p_1_32.py
+++ ex_1/p_1_32.py
@@ -1,136 +1,95 @@
-from decimal import Decimal, InvalidOperation
+# TODO
+# 1. function for reseting last input 
+# 2. function for clearing input 
+# 3. get last element at beginning 
+# 4. getters for certain elements of input 
+# 5. fcking unit tests
 
 def simple_calculator():
     """
-        Simple calculator
         Each input is done on separate line 
-        Supported actions: +, -, *, /
+        Only two numbers and one arithmetical expression between is supported   
+        To calculate result - input an equation sign
     """
-    v_operation_list = []
+    v_input = []
 
-    while True:
-        v_str = str(input())
-        process_input(io_operation_list=v_operation_list, i_input_str=v_str) 
-        print(v_operation_list)
+    while True: 
+        v_input.append(input().replace(" ", ""))
+        process_input(io_input=v_input)
+        # Debug
+        print("After processing: " + str(v_input))
 
-def process_input(io_operation_list: list, i_input_str: str):
+def process_input(io_input: list):
     """
-        Check input str and add to io_operation_list 
+        Runs checks and prints appropriate message for user 
+        Tries to evaluate the expression if possible 
     """
-    io_operation_list.append(i_input_str)
-    if check_operations(io_operation_list=io_operation_list):
-        calculate_result(io_operation_list=io_operation_list) 
+    print("Before processing: " + str(io_input))
+
+    if len(io_input) == 1:
+        if io_input[-1] == "":
+            del io_input[-1]
+            print("Enter a number:")
+        else:
+            try:
+                float(io_input[-1])
+            except ValueError:
+                del io_input[-1]
+                print("User input must be a number!")
+
+    elif len(io_input) == 2:
+        if len(io_input[-1]) > 0:
+            if io_input[-1] not in "+-*/" or len(io_input[-1]) > 1:
+                del io_input[-1]
+                print("User input must be an operator!")
+        else:
+            del io_input[-1]
+            print("Enter an operator. Supported operators are ( + | - | * | / ):")
+
+    elif len(io_input) == 3:
+        if io_input[-1] == "":
+            del io_input[-1]
+            print("Enter a number:")
+        else:
+            try:
+                if io_input[1] == "/" and float(io_input[-1]) == 0:
+                    del io_input[-1]
+                    print("Division by zero!")
+            except ValueError:
+                del io_input[-1]
+                print("User input must be a number!")
+
+    elif len(io_input) == 4:
+        if len(io_input[-1]) > 0 and io_input[-1] != "=":
+            del io_input[-1]
+            print("User input must be an equation sign (=)!")
+        elif len(io_input[-1]) == 0:
+            del io_input[-1]
+            print("Enter an equation sign to calculate the expression:")
+        else:
+            v_first_number = io_input[0]
+            v_operator = io_input[1]
+            v_second_number = io_input[2]
+            v_result = calculate_expression(
+                i_first_number=float(v_first_number),
+                i_second_number=float(v_second_number),
+                i_operator=v_operator
+            )
+            print("Result is: " + str(v_result))
+            while len(io_input) > 0:
+                del io_input[-1]
+
     else:
-        del io_operation_list[-1]
+        raise Exception("Something unknown occured!")
 
-def check_operations(io_operation_list: list) -> bool:
-    """
-        Returns True if all checks passed 
-        Returns False if otherwise
-    """
-    if check_decimal(io_operation_list=io_operation_list) and \
-        check_operator(io_operation_list=io_operation_list) and \
-        check_equity(io_operation_list=io_operation_list) and \
-        check_zero(io_operation_list=io_operation_list) and \
-        check_length(io_operation_list=io_operation_list):
-        return True
-    else:
-        return False
+def calculate_expression(i_first_number: float, i_second_number: float, i_operator: str) -> float:
+    if i_operator == "+":
+        return i_first_number + i_second_number 
+    elif i_operator == "-":
+        return i_first_number - i_second_number 
+    elif i_operator == "*":
+        return i_first_number * i_second_number
+    elif i_operator == "/":
+        return i_first_number / i_second_number
 
-def check_decimal(io_operation_list: list) -> bool:
-    """
-        Checks that first and third elements 
-        are convertable to decimal 
-    """
-    try:
-        Decimal(io_operation_list[0])
-    except IndexError:
-        pass
-    except InvalidOperation:
-        print("First argument must be a number!")
-        return False 
-    
-    try:
-        Decimal(io_operation_list[2])
-    except IndexError:
-        pass 
-    except InvalidOperation:
-        print("Second argument must be a number!")
-        return False 
-    
-    return True
-
-def check_operator(io_operation_list: list) -> bool:
-    """
-        2nd element must be one of those: +-*/
-    """
-    try:
-        v_operator = io_operation_list[1]
-    except IndexError:
-        return True 
-    
-    if len(v_operator) == 1 and v_operator in "+-*/":
-        return True
-    else:
-        print("Specified operation must be one of those: + | - | * | / |")
-        return False
-
-def check_equity(io_operation_list: list) -> bool:
-    """
-        4th element must be an equation sign
-    """
-    try:
-        v_equation = io_operation_list[3]
-    except IndexError:
-        return True 
-
-    if v_equation == "=":
-        return True
-    else:
-        print("Equation sign required!")
-        return False 
-
-def check_zero(io_operation_list: list) -> bool:
-    """
-        If division then no zero allowed for second argument
-    """
-    try:
-        v_operation = io_operation_list[1]
-        v_second_argument = io_operation_list[2]
-    except IndexError:
-        return True 
-    
-    if v_operation == "/" and int(v_second_argument) == 0:
-        print("Second division mustn't be zero on division!")
-        return False
-    else:
-        return True 
-
-def check_length(io_operation_list: list) -> bool:
-    """
-        Checks that operation list length doesn't exceed 5 elements
-    """
-    return True if len(io_operation_list) <= 5 else False
-
-def calculate_result(io_operation_list: list):
-    """
-        Calculates and prints result and deletes all elements from list
-    """
-    if len(io_operation_list) == 4:
-        v_first_argument = io_operation_list[0]
-        v_second_argument = io_operation_list[2]
-        v_operation = io_operation_list[1]
-
-        if v_operation == "+":
-            v_result = Decimal(v_first_argument) + Decimal(v_second_argument)
-        elif v_operation == "-":
-            v_result = Decimal(v_first_argument) - Decimal(v_second_argument)
-        elif v_operation == "*":
-            v_result = Decimal(v_first_argument) * Decimal(v_second_argument)
-        elif v_operation == "/":
-            v_result = Decimal(v_first_argument) / Decimal(v_second_argument)
-
-        print(str(v_result))
-        io_operation_list = []
-     
 simple_calculator()
