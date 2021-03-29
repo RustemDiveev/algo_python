13015ae01060715eb698e85ebb3a09108b220077
Rustem Diveev <rustem.diveev@gmail.com>
2021-03-29 11:41:21 +0300

    P1_34 - Генератор ошибок - перевод случайного символа в другой регистр

--- ex_1/p_1_34.py
+++ ex_1/p_1_34.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 def error_generator_remove_one_random_symbol(p_str: str) -> str:
     """
@@ -40,3 +40,30 @@ def error_generator_add_one_random_symbol_to_random_place(p_str: str) -> str:
     l_random_char = chr(l_random_char_idx)
 
     return p_str[:l_random_idx] + l_random_char + p_str[l_random_idx:]
+
+def error_generator_change_random_symbol_register(p_str: str) -> str:
+    """
+        Changes register of one of the random letter in a string and returns new string 
+        input:
+            p_str - input string
+        output: 
+            new string
+    """
+    """
+    swapcase() - changes register vice versa
+    isalpha() - checks that string consists only of letters
+    """
+    if len(p_str) == 0:
+        return p_str
+
+    l_char_idx_list = []
+    for i in range(0, len(p_str) - 1):
+        if p_str[i].isalpha():
+            l_char_idx_list.append(i)
+
+    if len(l_char_idx_list) == 0:
+        return p_str 
+    else:
+        l_random_idx = choice(l_char_idx_list)
+        l_converted_char = p_str[l_random_idx].swapcase()
+        return p_str[:l_random_idx] + l_converted_char + p_str[l_random_idx+1:]
