2518a56268a11c8de7d99ef5aceff5aac2dc934e
Rustem Diveev <rustem.diveev@gmail.com>
2021-05-24 20:12:00 +0300

    P1_36 - Подсунуть отрывок из русской литературы и сформировать список слов

--- ex_1/p_1_36.py
+++ ex_1/p_1_36.py
@@ -1,5 +1,7 @@
 from collections import Counter
 
+C_IGNORE_TOKENS_SET = {"-"}
+
 def print_counter(p_counter: Counter):
     """
         Helper for debugging 
@@ -7,20 +9,57 @@ def print_counter(p_counter: Counter):
         input:
             p_counter - counter to print
     """
-    l_file = open("ex_1/p_1_36_result.txt", "w+")
-    for i in p_counter.keys():
-        l_file.write(i + ": " + str(p_counter[i]) + "\n")
+    l_file = open("ex_1/p_1_36_result.txt", "w+", encoding="utf8")
+    for i in p_counter.most_common():
+        l_file.write(i[0] + ": " + str(i[1]) + "\n")
+
+def format_line(p_str: str) -> str:
+    """
+        Removes punctuation from a string
+        input:
+            p_str - input string 
+        output:
+            str
+    """
+    l_str = p_str.replace(".", "")
+    l_str = l_str.replace(",", "")
+    l_str = l_str.replace(";", "")
+    l_str = l_str.replace("?", "")
+    l_str = l_str.replace("!", "")
+    l_str = l_str.replace("\"", "")
+    l_str = l_str.lower()
+    return l_str
 
 def read_simple_file() -> Counter:
     """
         Reads file with space delimited words and returns counter of words
     """
     l_list = []
-    l_file = open("ex_1/p_1_36_txt_space_delimited_words.txt", "r")
+    l_file = open("ex_1/p_1_36/p_1_36_txt_space_delimited_words.txt", "r")
     l_lines = l_file.readlines()
     for line in l_lines:
         l_list += line.split()
     l_result = Counter(l_list)
     return l_result
 
-print_counter(p_counter=read_simple_file())
+def count_words_in_file(p_file_path: str) -> Counter:
+    """
+        Reads text file and returns counter of words
+        input:
+            p_file_path - path to text file 
+        output:
+            word counter
+    """
+    l_list = []
+    try:
+        l_file = open(p_file_path, "r", encoding="utf8")
+        l_lines = l_file.readlines()
+        for line in l_lines:
+            l_list += format_line(p_str=line).split()
+        l_result = Counter(elem for elem in l_list if elem not in C_IGNORE_TOKENS_SET)
+        return l_result
+    except FileNotFoundError:
+        print("Файл по пути " + p_file_path + " не обнаружен.")
+
+#print_counter(p_counter=read_simple_file()) 
+print_counter(p_counter=count_words_in_file(p_file_path="ex_1/p_1_36/_kryzhovnik.txt"))
