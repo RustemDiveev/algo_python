8c56004cc2ec9ad6359829a2f92043411518b07a
Rustem Diveev <rustem.diveev@gmail.com>
2021-06-03 23:05:39 +0300

    e2 - r1, r2 answered

---
+++ e2/r4.py
@@ -0,0 +1,11 @@
+# R-2.1 Give three examples of life-critical software applications.
+"""
+    1. Biomedical equipment
+    2. Automated car driving 
+    3. Rocket launch software
+"""
+# R-2.2 Give an example of a software application in which adaptability can mean
+# the difference between a prolonged lifetime of sales and bankruptcy.
+"""
+    Accounting system
+"""
