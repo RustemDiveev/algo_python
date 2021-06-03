6dd2a156c184cc8db04574f113a571e2c62907a5
Rustem Diveev <rustem.diveev@gmail.com>
2021-06-03 23:35:29 +0300

    e2r4 - Flower class

--- e2/r4.py
+++ e2/r4.py
@@ -9,3 +9,36 @@
 """
     Accounting system
 """
+
+class Flower:
+
+    def __init__(self, name: str, petals_cnt: int, price: float=0):
+        """
+            Constructor
+            name - str - name of flower 
+            petals_cnt - int - amount of petals 
+            price - float - price of flower
+        """
+        self._name = name
+        self._petals_cnt = petals_cnt 
+        self._price = price 
+
+    # Getters for instance members 
+    def get_name(self) -> str:
+        return self._name 
+
+    def get_petals_cnt(self) -> int:
+        return self._petals_cnt
+
+    def get_price(self) -> float:
+        return self._price 
+
+    # Setters for instance members
+    def set_name(self, name: str):
+        self._name = name 
+
+    def set_petals_cnt(self, petals_cnt: int):
+        self._petals_cnt = petals_cnt 
+
+    def set_price(self, price: float):
+        self._price = price
