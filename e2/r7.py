88f6facbb7dbe3ebc53a541f885863999cb8595e
Rustem Diveev <rustem.diveev@gmail.com>
2021-06-09 22:27:27 +0300

    e2r7 - modified class through inheritance

---
+++ e2/r7.py
@@ -0,0 +1,11 @@
+from e2.r6 import CreditCard_r6
+
+class CreditCard_r7(CreditCard_r6):
+
+    def __init__(self, customer, bank, acnt, limit, balance=0):
+        super().__init__(customer, bank, acnt, limit)
+        if isinstance(balance, (int, float)):
+            raise TypeError("Balance of credit card must be a number.")
+        if balance < 0:
+            raise ValueError("Balance mustn't be a negative number.")
+        self._balance = balance
