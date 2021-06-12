957d5da98a8c89452b1e9d8f54c1057ce7db8f4b
Rustem Diveev <rustem.diveev@gmail.com>
2021-06-12 15:16:09 +0300

    e2r10 - realization and test coverage

--- e2/r10.py
+++ e2/r10.py
@@ -5,7 +5,9 @@ class Vector_r10(Vector_r9):
         Added __neg__ method 
     """
     def __neg__(self):
+        # Useless, because it works for self
+        # l_vector = super().__init__(len(self))
         l_vector = Vector_r9(len(self))
         for i in range(len(self)):
-            l_vector[i] = -self[i]
+            l_vector[i] = self[i] * (-1)
         return l_vector 
